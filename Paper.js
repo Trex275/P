5fcb6be1191000b6c009d08448be385ae54bd3aa
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-03-10 21:22:53 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,11 @@
+class Paper {
+    constructor(){
+        var options={
+        isStatic:false,
+        restitution:0.3, 
+        friciton:0.5,
+        density:1.2
+        
+        }
+    }
+}
